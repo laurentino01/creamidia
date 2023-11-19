3e1c38850a2f01c38b8d786819b61a2c6010db03
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-11-18 22:35:49 -0300

    title testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
+import Testimonials from "@/components/testimonials/Testimonials";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <Testimonials />
     </main>
   );
 }
---
+++ src/components/testimonials/Testimonials.tsx
@@ -0,0 +1,23 @@
+import "./testimonials.style.css";
+
+export default function Testimonials() {
+  return (
+    <>
+      <section id="testimonials">
+        <div className="container">
+          <div className="content-area">
+            <div>
+              <p>nossos clientes</p>
+              <hr />
+              <h2>
+                Trabalhamos <br /> com os <span> maiores</span>
+              </h2>
+            </div>
+          </div>
+
+          <div></div>
+        </div>
+      </section>
+    </>
+  );
+}
