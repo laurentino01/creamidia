d6019e4bd7ea59b9a885be2648dbd3c8bd63ef0f
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-11-21 21:02:13 -0300

    refact authority

--- src/components/authority/Authority.tsx
+++ src/components/authority/Authority.tsx
@@ -1,33 +1,6 @@
 import "./authority.style.css";
 
-interface IListCardsData {
-  img: string;
-  paragraph: string;
-  name: string;
-}
-
 export default function Authority() {
-  const listCards: IListCardsData[] = [
-    {
-      img: "eagle.png",
-      paragraph:
-        "Essa produtora é top! Os editores são artistas, transformaram meus vídeos em puro sucesso. A atenção aos detalhes e a criatividade foram incríveis.",
-      name: "Eagle Trindade - Youtuber e empresário",
-    },
-    {
-      img: "johnny-klein.png",
-      paragraph:
-        "O trabalho da Crea Mídia sempre foi bom, eles sempre entregam os vídeos no prazo, Wesley é um excelente editor e mais ainda ele saber conduzir uma equipe.",
-      name: "INE - Youtuber e empresário",
-    },
-    {
-      img: "ine.webp",
-      paragraph:
-        "Primeiro contato que tive com eles me senti muito bem acolhido, 100% profissionais e com certeza surpreenderam minhas expectativas, qualquer alteração que eu pedi eles fizeram, gosto muito deles e me considero cliente fiel.",
-      name: "Johhny Klein - Ator e empresário ",
-    },
-  ];
-
   return (
     <section id="authority">
       <div className="container">
@@ -43,20 +16,57 @@ export default function Authority() {
         </div>
 
         <div className="card-area">
-          {listCards.map((item) => (
-            <div key={item.img} className="card">
-              <div
-                className="card-image"
-                style={{
-                  backgroundImage: `url('/images/authority/${item.img}')`,
-                }}
-              ></div>
+          <div className="card">
+            <div
+              className="card-image"
+              style={{
+                backgroundImage: `url('/images/authority/eagle.png')`,
+              }}
+            ></div>
 
-              <p>{item.paragraph} </p>
+            <p>
+              Essa produtora é top! Os editores <span> são artistas</span>,
+              transformaram meus vídeos em puro sucesso. A atenção aos detalhes
+              e a criatividade foram incríveis.
+            </p>
 
-              <h6>{item.name}</h6>
-            </div>
-          ))}
+            <h6>Eagle Trindade - Youtuber e empresário</h6>
+          </div>
+
+          <div className="card">
+            <div
+              className="card-image"
+              style={{
+                backgroundImage: `url('/images/authority/johnny-klein.png')`,
+              }}
+            ></div>
+
+            <p>
+              Primeiro contato que tive com eles me senti muito bem acolhido,
+              100% profissionais e com certeza surpreenderam minhas
+              expectativas,<span> qualquer alteração </span> que eu pedi eles
+              fizeram, gosto muito deles e me considero cliente fiel.
+            </p>
+
+            <h6>Johhny Klein - Ator e empresário</h6>
+          </div>
+
+          <div className="card">
+            <div
+              className="card-image"
+              style={{
+                backgroundImage: `url('/images/authority/ine.webp')`,
+              }}
+            ></div>
+
+            <p>
+              O trabalho da Crea Mídia sempre foi bom, eles sempre entregam os
+              vídeos <span> no prazo</span>, Wesley é um excelente editor e mais
+              ainda ele saber conduzir uma equipe.
+            </p>
+
+            <h6>INE - Youtuber e empresário</h6>
+          </div>
         </div>
       </div>
     </section>
