287fa571ff9cecf3874ed930c985ae12da063f63
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2024-01-02 09:58:28 -0300

    organização

--- src/app/homeComponents/authority/Authority.tsx
+++ src/app/homeComponents/authority/Authority.tsx
@@ -65,58 +65,6 @@ export default function Authority() {
               <h6> {cardItem.name}</h6>
             </div>
           ))}
-
-          {/* <div className="card">
-            <div
-              className="card-image"
-              style={{
-                backgroundImage: `url('/images/authority/eagle.png')`,
-              }}
-            ></div>
-
-            <p>
-              Essa produtora é top! Os editores <span> são artistas</span>,
-              transformaram meus vídeos em puro sucesso. A atenção aos detalhes
-              e a criatividade foram incríveis.
-            </p>
-
-            <h6>Eagle Trindade - Youtuber e empresário</h6>
-          </div>
-
-          <div className="card">
-            <div
-              className="card-image"
-              style={{
-                backgroundImage: `url('/images/authority/johnny-klein.png')`,
-              }}
-            ></div>
-
-            <p>
-              Primeiro contato que tive com eles me senti muito bem acolhido,
-              100% profissionais e com certeza surpreenderam minhas
-              expectativas,<span> qualquer alteração </span> que eu pedi eles
-              fizeram, gosto muito deles e me considero cliente fiel.
-            </p>
-
-            <h6>Johhny Klein - Ator e empresário</h6>
-          </div>
-
-          <div className="card">
-            <div
-              className="card-image"
-              style={{
-                backgroundImage: `url('/images/authority/ine.webp')`,
-              }}
-            ></div>
-
-            <p>
-              O trabalho da Crea Mídia sempre foi bom, eles sempre entregam os
-              vídeos <span> no prazo</span>, Wesley é um excelente editor e mais
-              ainda ele saber conduzir uma equipe.
-            </p>
-
-            <h6>INE - Youtuber e empresário</h6>
-          </div> */}
         </div>
       </div>
     </section>
